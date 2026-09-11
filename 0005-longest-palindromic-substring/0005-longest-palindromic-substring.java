1a516dd3b494749c85aeb604a0993e2abc1edc65
SUDANDHERAA S V <sudandheraasv@gmail.com>
2026-09-11 05:41:57 +0530

    Time: 16 ms (52.43%), Space: 43.3 MB (89.33%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n=s.length();
+        int start=0,maxlen=1;
+        for(int i=0;i<n;i++){
+            int left=i,right=i;
+            while(left>=0 && right<n && (s.charAt(left)==s.charAt(right))){
+                if(right - left +1 > maxlen){
+                    maxlen = right-left +1;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+            left=i;right=i+1;
+            while(left>=0 && right<n && s.charAt(left)==s.charAt(right)){
+                if(right - left+1 > maxlen){
+                    maxlen=right-left+1;
+                    start=left;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substring(start,start+maxlen);
+    }
+}
